540e6718c55d0dc25e42ec631827bd04a8f76135
fixprogram <fixprogram@mail.ru>
2020-11-16 22:50:14 +0300

    Initial commit with finished header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBKpyqiP9e_k430B9XN-F5c4uix0gZ3qg",
+  authDomain: "facebook-clone-6569f.firebaseapp.com",
+  databaseURL: "https://facebook-clone-6569f.firebaseio.com",
+  projectId: "facebook-clone-6569f",
+  storageBucket: "facebook-clone-6569f.appspot.com",
+  messagingSenderId: "688345996439",
+  appId: "1:688345996439:web:a666117f7f545431819dd2",
+};
